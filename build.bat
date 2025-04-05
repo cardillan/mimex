31e3d4226b44e7c740ea4dae08f347c98a5665eb
cardillan <122014763+cardillan@users.noreply.github.com>
2025-04-05 14:52:41 +0200

    Version 7

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 call gradlew.bat jar
-copy build\libs\mimexDesktop.jar "%APPDATA%\Mindustry\mods\mimexDesktop.jar" /y
+copy build\libs\mimexDesktop.jar "%APPDATA%\Mindustry\mods\mimexDesktop7.jar" /y
