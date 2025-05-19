5941d58152e128bb93fe704ce03f9031ebc3e3f7
cardillan <122014763+cardillan@users.noreply.github.com>
2025-05-19 22:07:37 +0200

    Update sounds and status effects

--- src/mimex/SoundsExtractor.java
+++ src/mimex/SoundsExtractor.java
@@ -8,6 +8,7 @@ public class SoundsExtractor extends MetadataExtractor {
 
     @Override
     public void extract() {
+        sbr.append("name").append(newLine);
         StreamSupport.stream(GlobalVars.soundNames.spliterator(), false).sorted().forEach(e -> sbr.append(e).append(newLine));
         writeToFile("sounds");
     }
