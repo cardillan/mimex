19ad4bc362a242ab15b7f1646636b3edc6aef59f
cardillan <122014763+cardillan@users.noreply.github.com>
2026-01-04 10:32:21 +0100

    Add isnull and privileged to vars

--- src/mimex/AlignmentExtractor.java
+++ src/mimex/AlignmentExtractor.java
@@ -7,10 +7,12 @@ public class AlignmentExtractor extends MetadataExtractor {
     @Override
     public void extract() {
         sbr.append("name")
+                .append(';').append("draw")
                 .append(newLine);
 
         for (String name : LStatement.nameToAlign.keys()) {
             sbr.append(name)
+                    .append(';').append(true)
                     .append(newLine);
         }
 
