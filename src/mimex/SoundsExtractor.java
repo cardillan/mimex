f00b814bc2528d89aa2587591591ac02bcabddcb
cardillan <122014763+cardillan@users.noreply.github.com>
2026-07-05 18:54:36 +0200

    Update to build 27358 (change to the sound files extraction)

--- src/mimex/SoundsExtractor.java
+++ src/mimex/SoundsExtractor.java
@@ -1,5 +1,10 @@
 package mimex;
 
+import arc.Core;
+import arc.audio.Sound;
+import arc.struct.Seq;
+import arc.util.Strings;
+import mindustry.gen.Sounds;
 import mindustry.logic.GlobalVars;
 
 import java.util.stream.StreamSupport;
@@ -9,7 +14,15 @@ public class SoundsExtractor extends MetadataExtractor {
     @Override
     public void extract() {
         sbr.append("name").append(newLine);
-        StreamSupport.stream(GlobalVars.soundNames.spliterator(), false).sorted().forEach(e -> sbr.append(e).append(newLine));
+        Seq<String> soundNames = new Seq<>();
+
+        for (var entry : Core.assets.getAllEntries(Sound.class, new Seq<>())) {
+            if (entry.value != Sounds.none && entry.value.file != null) {
+                soundNames.add(Strings.getFileNameWithoutExtension(entry.key));
+            }
+        }
+        soundNames.sort();
+        soundNames.each(e -> sbr.append(e).append(newLine));
         writeToFile("sounds");
     }
 }
