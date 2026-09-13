9e78eb71f6f06a1d1a496c94d6c1d5772f7e0c9a
cardillan <122014763+cardillan@users.noreply.github.com>
2026-09-13 15:46:45 +0200

    Try to load the mimex mod first Filter out content added by mods where possible

--- src/mimex/MindustryMetadataExtractor.java
+++ src/mimex/MindustryMetadataExtractor.java
@@ -17,10 +17,6 @@ public class MindustryMetadataExtractor extends Mod {
     private static final Map<String, String> classMap = new TreeMap<>();
 
     public MindustryMetadataExtractor() {
-        Log.info("MindustryMetadataExtractor constructor.");
-        Log.info("Mindustry version: " + Version.number + ", build: " + Version.build + ", revision: " + Version.revision + ".");
-        Fi fi = Core.files.local("build.txt");
-        fi.writeString("Mindustry version: " + Version.number + ", build: " + Version.build + ", revision: " + Version.revision + ".");
     }
 
     public static void registerClass(String key, String value) {
@@ -31,6 +27,11 @@ public class MindustryMetadataExtractor extends Mod {
     public void init() {
         super.init();
         if (Version.number == expectedVersion && Version.build >= minBuild && Version.build <= maxBuild) {
+            Log.info("[mimex] Mindustry version: " + Version.number + ", build: " + Version.build + ", revision: " + Version.revision + ".");
+
+            Fi fi = Core.files.local("build.txt");
+            fi.writeString("Mindustry version: " + Version.number + ", build: " + Version.build + ", revision: " + Version.revision + ".");
+
             new IconsExtractor().extract();
             new ColorsExtractor().extract();
             new BlocksExtractor().extract();
@@ -74,7 +75,7 @@ public class MindustryMetadataExtractor extends Mod {
 
             new ClassMapExtractor(classMap).extract();
         } else {
-            Log.warn(String.format("Mimex Mindustry version mismatch. Expected: %d (build %d to %d), got: %d (build %d)",
+            Log.warn(String.format("[mimex] Mindustry version mismatch. Expected: %d (build %d to %d), got: %d (build %d)",
                     expectedVersion, minBuild, maxBuild, Version.number, Version.build));
         }
     }
